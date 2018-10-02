c5b122566e70e3b2c7efaf6f7a558fc27a4a0b94
9929eric <43610219+9929eric@users.noreply.github.com>
2018-10-02 17:42:41 -0400

    Add files via upload

---
+++ hw3problem1.py
@@ -0,0 +1,10 @@
+disney_characters = ["simba", "ariel", "pumba", "flounder", "nala", "ursula", "scar", "flotsam", "timon"]
+for character in disney_characters:
+  if "u" in character:
+    print(character + " "+"U are so Uniquely U!")
+  elif "i" in character:
+    print(character+" "+"I bet you're Impressively Intelligent!")
+  elif "o" in character:
+    print(character+" "+"O My! How Original!");
+  else:
+    print(character+" "+"Ehh, a's and e's are so ordinary.");
